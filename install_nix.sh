980d66a06561ac203d2b59c588b34d1228c5e5b7
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-01-10 16:45:41 +0200

    dont use labels, for now

--- install_nix.sh
+++ install_nix.sh
@@ -17,12 +17,15 @@ sudo umount /mnt/ 2>/dev/null
 
 # prepare the drive
 parted "$drive" mklabel gpt
-parted -s -a opt "$drive" mkpart primary boot fat32 1MiB 261MiB
+parted -s -a opt "$drive" mkpart primary fat32 1MiB 261MiB
 parted -s -a opt "$drive" set 1 boot on
-parted -s -a opt "$drive" mkpart primary root ext4 261MiB 100%
+parted -s -a opt "$drive" mkpart primary ext4 261MiB 100%
+# parted -s -a opt "$drive" mkpart primary root ext4 261MiB 100%
 
 boot_partition="/dev/$(lsblk -l "$drive" | awk 'NR==3 {print $1}')"
 root_partition="/dev/$(lsblk -l "$drive" | awk 'NR==4 {print $1}')"
+echo $boot_partition
+echo $root_partition
 
 # parted seems to fail to format the drives... dunno why
 mkfs.fat -F 32 "$boot_partition"
