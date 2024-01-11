088f5385e2aff51ed7e041b1f88a386123214511
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-01-11 09:59:11 +0200

    pipe yes to avoid confirm prompts

--- install_nix.sh
+++ install_nix.sh
@@ -16,7 +16,7 @@ sudo umount /mnt/boot 2>/dev/null
 sudo umount /mnt/ 2>/dev/null
 
 # prepare the drive
-parted "$drive" mklabel gpt
+yes | parted "$drive" mklabel gpt
 parted -s -a opt "$drive" mkpart primary fat32 1MiB 261MiB
 parted -s -a opt "$drive" set 1 boot on
 parted -s -a opt "$drive" mkpart primary ext4 261MiB 100%
@@ -24,12 +24,10 @@ parted -s -a opt "$drive" mkpart primary ext4 261MiB 100%
 
 boot_partition="/dev/$(lsblk -l "$drive" | awk 'NR==3 {print $1}')"
 root_partition="/dev/$(lsblk -l "$drive" | awk 'NR==4 {print $1}')"
-echo $boot_partition
-echo $root_partition
 
 # parted seems to fail to format the drives... dunno why
 mkfs.fat -F 32 "$boot_partition"
-mkfs.ext4 "$root_partition"
+yes | mkfs.ext4 "$root_partition"
 
 # mount the partitions
 mount "$root_partition" /mnt || exit
