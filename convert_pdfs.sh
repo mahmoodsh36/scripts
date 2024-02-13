dc82b6a83b81ee60520cfeabc3148e4f0726541e
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-02-13 13:19:02 +0200

    fix typo

--- convert_pdfs.sh
+++ convert_pdfs.sh
@@ -10,6 +10,6 @@ mkdir -p "$BRAIN_DIR/resources/epubs" 2>/dev/null
 N=10
 for file in "$BRAIN_DIR/resources/"*.pdf; do
   ((i=i%N)); ((i++==0)) && wait
-  export -f echoFooBar
+  export -f handle_file
   timeout 180s sh -c "handle_file '$1'" &
 done
