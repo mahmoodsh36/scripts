24996bd95040def1f11a82fa9955e1d70e75ac37
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-02-13 12:40:57 +0200

    parallelize jobs

--- convert_pdfs.sh
+++ convert_pdfs.sh
@@ -1,8 +1,12 @@
-mkdir -p "$BRAIN_DIR/resources/epubs" 2>/dev/null
-for file in "$BRAIN_DIR/resources/"*.pdf; do
+handle_file() {
+  file="$1"
   newfile="$(basename "$file")"
   newfile="${newfile%.pdf}.epub"
   newfile_fullpath="$BRAIN_DIR/resources/epubs/$newfile" 
   [ ! -f "$newfile_fullpath" ] &&\
       ebook-convert "$file" "$newfile_fullpath" --enable-heuristics && echo "$newfile"
+}
+mkdir -p "$BRAIN_DIR/resources/epubs" 2>/dev/null
+for file in "$BRAIN_DIR/resources/"*.pdf; do
+  handle_file "$file" &
 done
