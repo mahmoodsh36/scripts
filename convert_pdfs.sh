2c897a5d89df4cb92f203a9149adcf64fff16436
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-02-13 13:36:31 +0200

    fix it

--- convert_pdfs.sh
+++ convert_pdfs.sh
@@ -1,15 +1,16 @@
-handle_file() {
-  file="$1"
-  newfile="$(basename "$file")"
-  newfile="${newfile%.pdf}.epub"
-  newfile_fullpath="$BRAIN_DIR/resources/epubs/$newfile" 
-  [ ! -f "$newfile_fullpath" ] &&\
-      ebook-convert "$file" "$newfile_fullpath" --enable-heuristics && echo "$newfile"
-}
 mkdir -p "$BRAIN_DIR/resources/epubs" 2>/dev/null
 N=10
 for file in "$BRAIN_DIR/resources/"*.pdf; do
   ((i=i%N)); ((i++==0)) && wait
-  export -f handle_file
-  timeout 180s sh -c "handle_file '$1'" &
+  # export -f handle_file
+  timeout 180s bash -c '
+    handle_file() {
+      file="$1"
+      newfile="$(basename "$file")"
+      newfile="${newfile%.pdf}.epub"
+      newfile_fullpath="$BRAIN_DIR/resources/epubs/$newfile"
+      [ ! -f "$newfile_fullpath" ] &&\
+          ebook-convert "$file" "$newfile_fullpath" --enable-heuristics && echo "$newfile"
+    }
+    handle_file '"\"$file\"" &
 done
