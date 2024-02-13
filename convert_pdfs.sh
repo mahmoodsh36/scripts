05b2281965dcb261dc891de4557a57165370b64c
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-02-13 10:39:16 +0200

    properly check file path

--- convert_pdfs.sh
+++ convert_pdfs.sh
@@ -2,6 +2,7 @@ mkdir -p "$BRAIN_DIR/resources/epubs" 2>/dev/null
 for file in "$BRAIN_DIR/resources/"*.pdf; do
   newfile="$(basename "$file")"
   newfile="${newfile%.pdf}.epub"
-  [ ! -f "$newfile" ] &&\
-      ebook-convert "$file" "$BRAIN_DIR/resources/epubs/$newfile" --enable-heuristics && echo "$newfile"
+  newfile_fullpath="$BRAIN_DIR/resources/epubs/$newfile" 
+  [ ! -f "$newfile_fullpath" ] &&\
+      ebook-convert "$file" "$newfile_fullpath" --enable-heuristics && echo "$newfile"
 done
