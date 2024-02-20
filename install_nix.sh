7157d0bb1e8c39c11d15e1cbb9b59c1bdafb2e62
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-02-20 18:19:21 +0200

    add partition labels

--- install_nix.sh
+++ install_nix.sh
@@ -16,26 +16,29 @@ sudo umount /mnt/boot 2>/dev/null
 sudo umount /mnt/ 2>/dev/null
 
 # prepare the drive
-yes | parted "$drive" mklabel gpt
-parted -s -a opt "$drive" mkpart primary fat32 1MiB 261MiB
+parted "$drive" mklabel gpt
+parted -s -a opt "$drive" mkpart myboot fat32 1MiB 261MiB
 parted -s -a opt "$drive" set 1 boot on
-parted -s -a opt "$drive" mkpart primary ext4 261MiB 100%
-# parted -s -a opt "$drive" mkpart primary root ext4 261MiB 100%
+parted -s -a opt "$drive" mkpart myswap ext4 261MiB 20GiB
+parted -s -a opt "$drive" mkpart myroot ext4 20GiBMiB 100%
 
 boot_partition="/dev/$(lsblk -l "$drive" | awk 'NR==3 {print $1}')"
-root_partition="/dev/$(lsblk -l "$drive" | awk 'NR==4 {print $1}')"
+swap_partition="/dev/$(lsblk -l "$drive" | awk 'NR==4 {print $1}')"
+root_partition="/dev/$(lsblk -l "$drive" | awk 'NR==5 {print $1}')"
 
 # parted seems to fail to format the drives... dunno why
 mkfs.fat -F 32 "$boot_partition"
+mkswap "$swap_partition"
 yes | mkfs.ext4 "$root_partition"
 
 # mount the partitions
 mount "$root_partition" /mnt || exit
 mkdir /mnt/boot 2>/dev/null
 mount "$boot_partition" /mnt/boot || exit
+swapon "$swap_partition"
 
 # nobody needs swap
-swapoff -L label
+# swapoff -L label
 
 # generate config (we need the hardware-configuration.nix file that is generated)
 mkdir -p /mnt/etc/
