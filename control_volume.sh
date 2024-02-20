24a86a42709ef93656241909d8f13f3fc7005f0e
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-02-20 17:46:34 +0200

    make script work with pactl

--- control_volume.sh
+++ control_volume.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env sh
+# set volume, e.g. ./control_volume +3%, sets volume for all sinks, with no arg simply prints volume of an arbitrary sink
+
+get_volume() {
+  pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
+}
 
 change="$1"
-
-sink=$(pacmd list-sinks | grep 'name:' | grep bluez | grep -o '<.*>' | tr -d '<>')
-
-if [ -z "$change" ]; then
-    volume=$(pacmd list-sinks | grep "$sink" -A10 | grep '^\svolume:' | awk '{print $5}')
-    echo "$volume"
-    exit 0
-fi
-
-pactl set-sink-volume $sink $change
+[[ ! -z "$change" ]] && pactl list sinks | grep Name | cut -d ':' -f2 | cut -c2- | while read sinkname; do pactl set-sink-volume "$sinkname" "$change"; done
+get_volume
