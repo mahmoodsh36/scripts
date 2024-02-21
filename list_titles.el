f76725e94581ed4bc72e4a34aff232a26752d7ec
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-02-21 22:53:34 +0200

    list org file titles

---
+++ list_titles.el
@@ -0,0 +1,10 @@
+#!/bin/sh
+":"; exec emacs --script "$0" "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
+(let ((enable-local-variables nil) ;; to avoid some errors when those use some stuff not in emacs by default
+      (org-startup-with-inline-images nil)) ;; to avoid errors
+  (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
+  (require 'setup-utils)
+  (require 'org)
+  (map-dir-files (format "%s/notes/" (getenv "BRAIN_DIR")) (list (lambda () (princ (format "%s\n" (org-get-title))))))
+
+  )
