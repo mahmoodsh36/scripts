94f4e3c414ddb34f364da71e3db19c58a3e11b37
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-02-25 06:25:06 +0200

    dont try to fetch lyrics

--- mpv_lyrics_file.sh
+++ mpv_lyrics_file.sh
@@ -2,5 +2,4 @@
 # return the filepath of lyrics file for the currently playing mpv track
 
 out=$(echo '{ "command": ["get_property", "path"] }' | socat - /tmp/mpv_socket | jq -j .data);
-spotdl_lyrics.sh "$out"
 echo "${out%.mp3}.lrc"
