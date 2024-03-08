a4223864cfe95c715455326202566bee8c8353e7
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-03-08 21:03:17 +0200

    script to eval elisp expressions inside the context of my emacs config

---
+++ emacs_eval.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+read -r -d '' myhead <<- EOF
+(let ((enable-local-variables nil) ;; to avoid some errors when those use some stuff not in emacs by default
+;;(org-startup-with-inline-images nil) ;; to avoid errors
+      (warning-minimum-level :emergency)) ;; reduce warnings
+  (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
+  (require 'setup-constants)
+  (require 'setup-utils)
+  (require 'setup-elpaca)
+  (require 'setup-org)
+  (elpaca-wait)
+EOF
+emacs --batch --eval "$myhead$1)"
