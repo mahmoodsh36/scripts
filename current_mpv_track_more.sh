46f5ad8737f191c74bed77232f1c2fb26fe8f937
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-03-16 12:43:25 +0200

    should be -S not -f, for socket

--- current_mpv_track_more.sh
+++ current_mpv_track_more.sh
@@ -1,2 +1,2 @@
 #+/bin/env sh
-[ -f /tmp/mpv_socket ] && (name=$(echo "{ \"command\": [\"get_property\", \"metadata\"] }" | socat - /tmp/mpv_socket | jq -j ".data | .title + \" - \" + .artist + \" \" + .track + \" \" + .disc"); subtitles=$(echo '{ "command": ["get_property", "sub-text"] }' | socat - /tmp/mpv_socket | jq -j '.data?'); echo -n "$name $subtitles")
+[ -S /tmp/mpv_socket ] && (name=$(echo "{ \"command\": [\"get_property\", \"metadata\"] }" | socat - /tmp/mpv_socket | jq -j ".data | .title + \" - \" + .artist + \" \" + .track + \" \" + .disc"); subtitles=$(echo '{ "command": ["get_property", "sub-text"] }' | socat - /tmp/mpv_socket | jq -j '.data?'); echo -n "$name $subtitles")
