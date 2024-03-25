12a86987075ad0c0a0d0aa9542aa853f396ad8a2
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-03-25 20:22:51 +0200

    increase timeout, generate with and without heuristics

--- convert_pdfs.sh
+++ convert_pdfs.sh
@@ -3,14 +3,16 @@ N=10
 for file in "$BRAIN_DIR/resources/"*.pdf; do
   ((i=i%N)); ((i++==0)) && wait
   # export -f handle_file
-  timeout 180s bash -c '
+  timeout 500s bash -c '
     handle_file() {
       file="$1"
       newfile="$(basename "$file")"
       newfile="${newfile%.pdf}.epub"
       newfile_fullpath="$BRAIN_DIR/resources/epubs/$newfile"
-      [ ! -f "$newfile_fullpath" ] &&\
-          ebook-convert "$file" "$newfile_fullpath" --enable-heuristics && echo "$newfile"
+      if [ ! -f "$newfile_fullpath" ] && (
+        ebook-convert "$file" hu_"$newfile_fullpath" --enable-heuristics && echo "$newfile"
+        ebook-convert "$file" nohu_"$newfile_fullpath" && echo "$newfile"
+      )
     }
     handle_file '"\"$file\"" &
 done
