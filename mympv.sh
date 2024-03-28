a029d9a0301e843971b022a05fd5367d5ce84c37
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-03-28 22:37:37 +0200

    pass args with double quotes

--- mympv.sh
+++ mympv.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 # a wrapper for mpv
-[ ! -S /tmp/mpv_socket ] && (mpv --input-ipc-server=/tmp/mpv_socket $@; rm /tmp/mpv_socket) || mpv $@
+[ ! -S /tmp/mpv_socket ] && (mpv --input-ipc-server=/tmp/mpv_socket "$@"; rm /tmp/mpv_socket) || mpv "$@"
