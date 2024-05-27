b3a70db8fb0d8b1e300d218ba62cb30b650779ca
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-05-27 22:04:05 +0300

    use mkv

--- webcam.sh
+++ webcam.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-# record from webcam
-ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -f alsa -i default -c:v libx265 -crf 28 -c:a aac -b 300k $(date | tr ' ' '_')_webcam.mp4
+# record from webcam, use mkv because it can handle interruptions and still be playable, unlike mp4
+ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -f alsa -i default -c:v libx265 -crf 28 -c:a aac -b 300k $(date | tr ' ' '_')_webcam.mkv
