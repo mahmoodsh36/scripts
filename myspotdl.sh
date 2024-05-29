9a4d96121643b555a014bf9ddf6397cb00fe5002
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-05-29 20:19:22 +0300

    extracted the unique_file function into its own file

--- myspotdl.sh
+++ myspotdl.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env sh
 
-unique_file() {
-  name=$1
-  ext=$2
-  if [[ -e "$name.$ext" || -L "$name.$ext" ]] ; then
-    i=0
-    while [[ -e "$name-$i.$ext" || -L "$name-$i.$ext" ]] ; do
-      let i++
-    done
-    name=$name-$i
-  fi
-  echo "$name.$ext"
-}
-
 cd "$HOME/music/"
 for arg in "$@"; do
   spotdl --overwrite skip\
@@ -20,8 +7,8 @@ for arg in "$@"; do
     download "$arg"\
     --output '{artist}/{album}/{title}--{track-id}'\
     --print-errors\
-    --save-errors "$(unique_file "$arg" errors.spotdl)"\
-    --save-file "$(unique_file "$arg" spotdl)"\
+    --save-errors "$(unique_file.sh "$arg" errors.spotdl)"\
+    --save-file "$(unique_file.sh "$arg" spotdl)"\
     --lyrics --max-retries 10 --add-unavailable\
     --generate-lrc --fetch-albums\
     --yt-dlp-args '--cookies-from-browser chrome' # --scan-for-songs
---
+++ unique_file.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# generate a unique filename, doesnt create the file itself
+# usage: ./<script> [filename] [extension]
+
+unique_file() {
+  name=$1
+  ext=$2
+  if [[ -e "$name.$ext" || -L "$name.$ext" ]] ; then
+    i=0
+    while [[ -e "$name-$i.$ext" || -L "$name-$i.$ext" ]] ; do
+      let i++
+    done
+    name=$name-$i
+  fi
+  echo "$name.$ext"
+}
+
+# ext=""
+# [ ! -z "$2" ] && ext="$2"
+ext="$2"
+filename="$1"
+[ -z "$filename" ] && filename=my_unique
+unique_file "$filename" "$ext"
