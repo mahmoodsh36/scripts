e5146b7dc5aa5c029078df3f8b75a2bf67dc76c1
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-05-29 20:19:35 +0300

    use a unique socket file every time

--- mympv.sh
+++ mympv.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 # a wrapper for mpv
-[ ! -S /tmp/mpv_socket ] && (mpv --input-ipc-server=/tmp/mpv_socket "$@"; rm /tmp/mpv_socket) || mpv "$@"
+# [ ! -S /tmp/mpv_socket ] && (mpv --input-ipc-server=/tmp/mpv_socket "$@"; rm /tmp/mpv_socket) || mpv "$@"
+socket_dir=$HOME/data/mpv_data/sockets
+socket_file=$(cd "$socket_dir"; unique_file.sh "mpv" "socket")
+notify-send $socket_file
+full_socket_file_path="/$socket_dir/$socket_file"
+mpv --input-ipc-server="$full_socket_file_path" "$@"; rm "$full_socket_file_path" 
