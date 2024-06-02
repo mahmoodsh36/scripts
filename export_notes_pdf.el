66676e957fc0576942299f50a18778ea8c59d41c
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-06-02 23:19:13 +0300

    export all to pdfs

---
+++ export_notes_pdf.el
@@ -0,0 +1,15 @@
+#!/bin/sh
+":"; exec emacs --script "$0" "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
+(let ((enable-local-variables nil) ;; to avoid some errors when those use some stuff not in emacs by default
+      (org-startup-with-inline-images nil) ;; to avoid errors
+      (warning-minimum-level :emergency)) ;; reduce warnings
+  (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
+  (require 'setup-utils)
+  (require 'setup-constants)
+  (require 'setup-android)
+  (require 'setup-elpaca)
+  (require 'setup-org)
+  (require 'setup-blk)
+  (elpaca-wait)
+  (let ((org-export-restrict nil))
+    (export-all-org-files :pdf-p t)))
