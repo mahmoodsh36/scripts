67398a08846276c9b590d106796a9d8a493331a9
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-06-24 19:39:44 +0300

    dont use max retries, lower thread count

--- myspotdl.sh
+++ myspotdl.sh
@@ -3,13 +3,18 @@
 cd "$HOME/music/"
 for arg in "$@"; do
   spotdl --overwrite skip\
-    --threads 36\
+    --threads 4\
     download "$arg"\
     --output '{artist}/{album}/{title}--{track-id}'\
     --print-errors\
     --save-errors "$(unique_file.sh "$arg" errors.spotdl)"\
     --save-file "$(unique_file.sh "$arg" spotdl)"\
-    --lyrics --max-retries 10 --add-unavailable\
-    --generate-lrc --fetch-albums\
+    --lyrics --add-unavailable\
+    --generate-lrc\
+    --fetch-albums\
     --yt-dlp-args '--cookies-from-browser chrome' # --scan-for-songs
+    # --audio youtube-music\
+    # --only-verified-results\
+    # --ytm-data\
+    #--max-retries 10\
 done
