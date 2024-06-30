f453e3ff9829b3533a62b9cae331c9f2a3e1ec65
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-06-30 15:51:22 +0300

    rewrite args passed to spotdl

--- myspotdl.sh
+++ myspotdl.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env sh
 
 cd "$HOME/music/"
-for arg in "$@"; do
-  spotdl --overwrite skip\
-    --threads 4\
-    download "$arg"\
-    --output '{artist}/{album}/{title}--{track-id}'\
-    --print-errors\
-    --save-errors "$(unique_file.sh "$arg" errors.spotdl)"\
-    --save-file "$(unique_file.sh "$arg" spotdl)"\
-    --lyrics --add-unavailable\
-    --generate-lrc\
-    --fetch-albums\
-    --yt-dlp-args '--cookies-from-browser chrome' # --scan-for-songs
-    # --audio youtube-music\
+spotdl --overwrite skip\
+       --threads 4\
+       download "$@"\
+       --output '{artist}/{album}/{title}--{track-id}'\
+       --print-errors\
+       --save-errors "$(unique_file.sh "$arg" errors.spotdl)"\
+       --save-file "$(unique_file.sh "$arg" spotdl)"\
+       --lyrics --add-unavailable\
+       --generate-lrc\
+       --fetch-albums\
+       --config\
+       --yt-dlp-args '--cookies-from-browser chrome'
+# --scan-for-songs
+# --audio youtube-music\
     # --only-verified-results\
     # --ytm-data\
     #--max-retries 10\
-done
