fc188ecb7426dceffb53d3c0d7650abf68115bca
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-06-30 16:04:33 +0300

    unset a dynamic variable that was ccausing errors

--- emacs_eval.sh
+++ emacs_eval.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 read -r -d '' myhead <<- EOF
 (let ((enable-local-variables nil) ;; to avoid some errors when those use some stuff not in emacs by default
-      (org-startup-with-inline-images nil) ;; to avoid errors
       (warning-minimum-level :emergency)) ;; reduce warnings
 
   (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
