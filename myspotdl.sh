150fdf8eb7ecf883f44a28019a9d675147b3a22b
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-08-29 18:50:47 +0300

    minor change

--- myspotdl.sh
+++ myspotdl.sh
@@ -5,7 +5,7 @@ spotdl --overwrite skip --threads 4 download "$@" \
   --output '{artist}/{album}/{title}--{track-id}' \
   --print-errors --save-errors "$(unique_file.sh "$@" errors.spotdl)" \
   --save-file "$(unique_file.sh "$@" spotdl)" \
-  --lyrics --add-unavailable --generate-lrc --fetch-albums --config --yt-dlp-args '--cookies-from-browser chrome'
+  --lyrics --add-unavailable --generate-lrc --fetch-albums --config --yt-dlp-args '--cookies-from-browser firefox'
 # --scan-for-songs
 # --audio youtube-music\
 # --only-verified-results\
