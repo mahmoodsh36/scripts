699dcea1758a68d35d3d77171242689577a729ea
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-09-25 22:16:24 +0300

    add script for taking scrots on wayland

---
+++ myscrot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+file="$(echo ~/data/images/scrots/`date | tr " " "_"`.png)"
+arg="$1" # supply any value to make it select an area of the screen
+if [ -z "$arg" ]; then
+    notif_str="screenshot taken"; grim "$file" && echo -n "$file" | wl-copy && notify-send "$notif_str" && sxiv "$file"
+else
+    notif_str="image of selection taken"; sleep 0.2; slurp | grim -g - "$file" && echo -n "$file" | wl-copy && notify-send "$notif_str" && sxiv "$file"
+fi
