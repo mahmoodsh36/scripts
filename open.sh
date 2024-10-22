c172882e0583fd151cba39585e1dc5c5b68ae62f
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-10-22 15:50:59 +0300

    make it handle symlinks

--- open.sh
+++ open.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env sh
 
+get_mime() {
+  echo $(file --mime-type "$1" | rev | cut -d ':' -f1 | rev | cut -c2-)
+}
+
 file="$1"
-mime=$(file --mime-type "$1" | rev | cut -d ':' -f1 | rev | cut -c2-)
+mime="$(get_mime "$file")"
+if [ "$mime" = "inode/symlink" ]; then
+  # resolve the symlink
+  file=$(readlink -f "$file")
+  mime="$(get_mime "$file")"
+fi
 
 case "$mime" in
   application/pdf)
