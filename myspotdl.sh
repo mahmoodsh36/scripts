99754a2f0cb175c86d38c4ead3c89d23dd3690a2
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-10-27 18:46:04 +0200

    my my spotify dev client id/secret

--- myspotdl.sh
+++ myspotdl.sh
@@ -5,10 +5,15 @@ spotdl --overwrite skip --threads 4 download "$@" \
   --output '{artist}/{album}/{title}--{track-id}' \
   --print-errors --save-errors "$(unique_file.sh "$@" errors.spotdl)" \
   --save-file "$(unique_file.sh "$@" spotdl)" \
-  --lyrics --add-unavailable --generate-lrc --fetch-albums --config --yt-dlp-args '--cookies-from-browser firefox'
+  --client-id "$SPOTIFY_CLIENT_ID"\
+  --client-secret "$SPOTIFY_CLIENT_SECRET"\
+  --user-auth\
+  --lyrics --add-unavailable --generate-lrc --fetch-albums --config
+  # --yt-dlp-args '--cookies-from-browser firefox'
+  # --cookie-file "$HOME/dl/cookies.txt"
+  # --cookie-file '~/.mozilla/firefox/17f1v26k.default-release/cookies.sqlite'
 # --scan-for-songs
 # --audio youtube-music\
 # --only-verified-results\
 # --ytm-data\
 #--max-retries 10\
-
