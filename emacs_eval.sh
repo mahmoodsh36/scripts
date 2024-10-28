adf021236f87cbc266c946fc3059292c29549229
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-10-29 00:32:35 +0200

    require more files, turn on debug

--- emacs_eval.sh
+++ emacs_eval.sh
@@ -3,13 +3,16 @@ read -r -d '' myhead <<- EOF
 (let ((enable-local-variables nil) ;; to avoid some errors when those use some stuff not in emacs by default
       (warning-minimum-level :emergency)) ;; reduce warnings
 
+  (toggle-debug-on-error)
   (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
   (require 'setup-utils)
   (require 'setup-constants)
   (require 'setup-android)
   (require 'setup-elpaca)
+  (require 'setup-other)
   (require 'setup-org)
   (require 'setup-packages)
+  (require 'setup-theme)
   (require 'setup-blk)
   (elpaca-wait)
 EOF
