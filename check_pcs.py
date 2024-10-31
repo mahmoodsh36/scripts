44547d7ebfa0e7522e92e7242d91f7beddd5a3e5
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-10-31 18:35:26 +0200

    more work on locating machines

--- check_pcs.py
+++ check_pcs.py
@@ -2,18 +2,28 @@
 import os
 import re
 import subprocess
+import sys
+
+job = sys.argv[1] if sys.argv[1:] else None
 
 MYCOMPUTERS = os.environ['MYCOMPUTERS']
 KEY = os.environ['MAIN_KEY']
+MAIN_SERVER_IP = os.environ['MAIN_SERVER_IP']
 DIFF_CMD = """rsync --progress -a -n ~/brain {}:/home/mahmooz/ -e 'ssh -i ~/brain/keys/hetzner1' --exclude '.git'"""
 
 def get_local_ip():
     out = subprocess.check_output(['ip', 'addr'])
-    matches = re.search("inet (.*)/24", out.decode()).groups()
+    matches = re.findall("inet (.*)/", out.decode())
     return [match for match in matches if match != "127.0.0.1"][0]
 
+def get_local_mac():
+    rx = re.compile(r"link/ether (.*) brd .*\n\s+inet",re.MULTILINE)
+    out = subprocess.check_output(['ip', 'addr']).decode()
+    matches = rx.findall(out)
+    return matches[0]
+
 local_ip = get_local_ip()
-print(f'local ip {local_ip}')
+# print(f'local ip {local_ip}')
 
 def run_remote_command(ip, cmd):
     out = subprocess.check_output(['ssh', '-i', KEY, ip, cmd]).decode()
@@ -28,19 +38,60 @@ def get_ip_using_mac(mac):
             if tokens[4] == mac:
                 return tokens[0]
 
+# returns whether a ping succeeded
+def try_ping(hostname):
+    hostname = addr_remove_port(hostname)
+    out = subprocess.run(['ping', '-c', '1', hostname], stdout = subprocess.DEVNULL)
+    if out.returncode == 0:
+        return True
+    return False
+
+def addr_remove_port(myaddr):
+    return myaddr.split(':')[0] # get rid of port
+
 machines = []
 for line in MYCOMPUTERS.split('::'):
     tokens = line.split(',')
-    mac, name = tokens[0], tokens[1]
+    possible_addresses = tokens[0].split('|')
+    mac = None
+    ip = None
+    for candidate in possible_addresses:
+        if '.' in candidate:
+            if try_ping(candidate):
+                ip = candidate
+                break
+        else:
+            ip = get_ip_using_mac(candidate)
+            if ip:
+                mac = candidate
+                break
+    name = tokens[1]
     is_main_storage = tokens[2] if tokens[2:] else None
     machines.append({
         'mac': mac,
         'name': name,
         'is_main_storage': is_main_storage,
-        'ip': get_ip_using_mac(mac)
+        'ip': ip,
+        'candidate_addresses': possible_addresses
     })
 
 for machine in machines:
-    if machine['ip']:
-        out = subprocess.check_output(DIFF_CMD.format(machine['ip']), shell=True).decode()
-        print(f'{machine["name"]}: {out}')
+    if job == 'diff':
+        if machine['ip']:
+            if get_local_mac() not in machine['candidate_addresses']:
+                out = subprocess.check_output(DIFF_CMD.format(machine['ip']), shell=True).decode()
+                print(f'{machine["name"]}: {out}')
+    if job == 'print_diff_cmd':
+        if machine['ip']:
+            if get_local_mac() not in machine['candidate_addresses']:
+                print(DIFF_CMD.format(machine['ip']))
+    if job == 'print_machines_json':
+        if machine['ip']:
+            if get_local_mac() not in machine['candidate_addresses']:
+                out = subprocess.check_output(DIFF_CMD.format(machine['ip']), shell=True).decode()
+                print(f'{machine["name"]}: {out}')
+    if job == 'print_machines':
+        if machine['ip']:
+            if get_local_mac() not in machine['candidate_addresses']:
+                out = subprocess.check_output(DIFF_CMD.format(machine['ip']), shell=True).decode()
+                print(f'{machine["name"]},{machine["ip"]}')
