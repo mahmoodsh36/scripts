08efe1abf53d9a1b6b24cfc3abc29e1264e5892b
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-12-18 07:00:05 +0200

    use dmenu.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 cd ~/work/
-sh -c "$(echo $PATH | tr ':' '\n' | while read the_dir; do ls --color=no "$the_dir"; done | sort -u | menu.py)"
+sh -c "$(echo $PATH | tr ':' '\n' | while read the_dir; do ls --color=no "$the_dir"; done | sort -u | dmenu.sh)"
