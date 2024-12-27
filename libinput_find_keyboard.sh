d88ed3eb451f02ff1e3084c91efbf20ff70dc9cb
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-12-27 13:48:36 +0200

    make it work better

--- libinput_find_keyboard.sh
+++ libinput_find_keyboard.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
-libinput list-devices | awk '/AT Translated/{found=1; next} found == 1 {print $2; exit}'
+# libinput list-devices | awk '/AT Translated/{found=1; next} found == 1 {print $2; exit}'
+symlink="$(find /dev/input/by-path -maxdepth 1 -name '*event-kbd' | head -1)"
+readlink -f "$symlink"
