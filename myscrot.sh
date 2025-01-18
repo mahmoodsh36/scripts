2ffe9a665b44bf2eec0784f73ce0d3389169eab4
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-01-18 18:18:21 +0200

    minor change

--- myscrot.sh
+++ myscrot.sh
@@ -3,7 +3,7 @@
 file="$(echo ~/data/images/scrots/`date | tr " " "_"`.png)"
 arg="$1" # supply any value to make it select an area of the screen
 if [ -z "$arg" ]; then
-    notif_str="screenshot taken"; grim "$file" && echo -n "$file" | wl-copy && notify-send "$notif_str" && sxiv "$file"
+    notif_str="screenshot taken"; grim "$file" && echo -n "$file" | wl-copy && imv "$file" && notify-send "$notif_str"
 else
-    notif_str="image of selection taken"; sleep 0.2; slurp | grim -g - "$file" && echo -n "$file" | wl-copy && notify-send "$notif_str" && sxiv "$file"
+    notif_str="image of selection taken"; sleep 0.2; slurp | grim -g - "$file" && echo -n "$file" | wl-copy && imv "$file" && notify-send "$notif_str"
 fi
