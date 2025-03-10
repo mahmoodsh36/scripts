d7acbbab59e0b2264175f1aea21d0f611464bc61
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-03-10 23:54:09 +0200

    minor changes

--- web_bookmarks.sh
+++ web_bookmarks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-choice=$(cat ~/.config/bookmarks | wofi --dmenu)
+choice=$(cat ~/.config/bookmarks | dmenu.sh)
 if [ ! -z "$choice" ]; then
     url=$(echo $choice | cut -d ' ' -f1)
     $BROWSER "$url"
