1b47a467bbe29aa5f575382feb01b97c3a1d5b62
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-08-07 11:30:20 +0300

    alternative music sorting script

--- play_dir_as_album.sh
+++ play_dir_as_album.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 dir="$1"
 [ -z "$dir" ] && dir="."
-list_dir_as_album.py "$dir" | mympv.sh --playlist=-
+# list_dir_as_album.py "$dir" | mympv.sh --playlist=-
+sort_music.py -q "$dir" | mympv.sh --playlist=-
