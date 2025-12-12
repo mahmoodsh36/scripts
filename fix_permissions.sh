23e2b69004a607556668aee7fbb5d0aea1025423
mahmoodsh36 <mahmod.m2015@gmail.com>
2025-12-12 03:42:07 +0200

    more reasonable permissions

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
-[ -z "$1" ] && exit
-find "$1" -type d -print0 | xargs -0 chmod 0775
-find "$1" -type f -print0 | xargs -0 chmod 0664
+
+[ -z "$1" ] && exit 1
+
+find "$1" -type d -exec chmod 0755 {} +
+find "$1" -type f -exec chmod 0644 {} +
